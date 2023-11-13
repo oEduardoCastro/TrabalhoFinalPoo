12511b93bc64e666fcf4bc64d899999cfac95a69
oEduardoCastro <oeducastro@gmail.com>
2023-11-13 12:29:10 -0300

    construtores do Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,2 +1,29 @@
 public class Cliente {
+    private String nome;
+    private double matricula;
+    private double telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(double matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
 }
