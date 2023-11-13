ec66616d7323b3ecabfa61229d6dbf44c9a9d005
oEduardoCastro <oeducastro@gmail.com>
2023-11-13 12:25:37 -0300

    classes biblioteca, cliete e livro criadas

---
+++ Livro.java
@@ -0,0 +1,48 @@
+public class Livro {
+    private String nome;
+    private double isbn;
+    private String autor;
+    private String editora;
+    private double dataDeLancamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(double isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public double getDataDeLancamento() {
+        return dataDeLancamento;
+    }
+
+    public void setDataDeLancamento(double dataDeLancamento) {
+        this.dataDeLancamento = dataDeLancamento;
+    }
+
+}
